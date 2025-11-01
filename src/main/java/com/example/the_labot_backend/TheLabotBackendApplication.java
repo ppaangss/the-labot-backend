34a4157020b1604c2861a05ed3288b5d9505c602
rmsckd1640 <rmsckd1640@gmail.com>
2025-11-01 17:53:21 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/example/the_labot_backend/TheLabotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.the_labot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLabotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLabotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/the_labot_backend/TheLabotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the_labot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLabotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
