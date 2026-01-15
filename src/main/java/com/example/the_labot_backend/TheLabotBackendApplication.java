ed71330f56f91dd3127d2f44f5e297bbaf1e07b1
ppaangss <ppaangss@naver.com>
2026-01-15 20:52:47 +0900

    테스트

--- src/main/java/com/example/the_labot_backend/TheLabotBackendApplication.java
+++ src/main/java/com/example/the_labot_backend/TheLabotBackendApplication.java
@@ -16,7 +16,7 @@ public class TheLabotBackendApplication {
 	public void init() {
 		// 애플리케이션 전역에서 TimeZone을 한국 시간으로 설정
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-		System.out.println("현재 시간대: " + TimeZone.getDefault().getID());
+		System.out.println("현재 시간대: " + TimeZone.getDefault().getID()) ;
 	}
 
 }
